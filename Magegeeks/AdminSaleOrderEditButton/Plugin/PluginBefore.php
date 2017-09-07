79ded4c0133f08a1a616eed72ce367094eb97bab
Deepak Mankotia <mss.deepakmankotia@gmail.com>
2017-09-07 11:36:43 +0530

    Code cleanup

--- Magegeeks/AdminSaleOrderEditButton/Plugin/PluginBefore.php
+++ Magegeeks/AdminSaleOrderEditButton/Plugin/PluginBefore.php
@@ -19,8 +19,7 @@ class PluginBefore
     	$this->_request = $context->getRequest();
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $this->_backendUrl = $objectManager->get('\Magento\Framework\App\Response\RedirectInterface'); 
-        $url = $this->_backendUrl->getRedirectUrl();//$this->_backendUrl->getUrl("adminhtml/sales/order");
-
+        $url = $this->_backendUrl->getRedirectUrl();
     	if($this->_request->getFullActionName() == 'sales_order_edit_index'){
 	          $buttonList->add(
 	            'return',
